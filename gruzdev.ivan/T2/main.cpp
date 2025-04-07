8c0dbf36634f919e4c07c0aa82f7d606fcf9c148
Груздев <gruzdev_i06@mail.ru>
2025-04-07 19:29:10 +0300

    Fixed problems with whitespace

--- gruzdev.ivan/T2/main.cpp
+++ gruzdev.ivan/T2/main.cpp
@@ -8,45 +8,34 @@
 #include <iomanip>
 #include <limits>
 
-
-
-
-namespace nspace
-{
-    struct DataStruct
-    {
+namespace nspace {
+    struct DataStruct {
         char key1;
         std::pair<long long, unsigned long long> key2;
         std::string key3;
     };
 
-    struct DelimiterIO
-    {
+    struct DelimiterIO {
         char exp;
     };
 
-    struct CharIO
-    {
+    struct CharIO {
         char& ref;
     };
 
-    struct RationalIO
-    {
+    struct RationalIO {
         std::pair<long long, unsigned long long>& ref;
     };
 
-    struct StringIO
-    {
+    struct StringIO {
         std::string& ref;
     };
 
-    struct LabelIO
-    {
+    struct LabelIO {
         std::string exp;
     };
 
-    class iofmtguard
-    {
+    class iofmtguard {
     public:
         iofmtguard(std::basic_ios<char>& s);
         ~iofmtguard();
@@ -69,9 +58,7 @@ namespace nspace
     bool compare(const DataStruct& a, const DataStruct& b);
 }
 
-
-int main()
-{
+int main() {
     using namespace nspace;
 
     std::vector<DataStruct> data;
@@ -103,40 +90,32 @@ int main()
     );
 
     return 0;
-
 }
 
-namespace nspace
-{
-    std::istream& operator>>(std::istream& in, DelimiterIO&& dest)
-    {
+namespace nspace {
+    std::istream& operator>>(std::istream& in, DelimiterIO&& dest) {
         std::istream::sentry sentry(in);
-        if (!sentry)
-        {
+        if (!sentry) {
             return in;
         }
 
         char c = '0';
         in >> c;
-        if (in && (c != dest.exp))
-        {
+        if (in && (c != dest.exp)) {
             in.setstate(std::ios::failbit);
         }
         return in;
     }
 
-    std::istream& operator>>(std::istream& in, CharIO&& dest)
-    {
+    std::istream& operator>>(std::istream& in, CharIO&& dest) {
         std::istream::sentry sentry(in);
-        if (!sentry)
-        {
+        if (!sentry) {
             return in;
         }
 
         char quote = '0';
         in >> quote;
-        if (quote != '\'')
-        {
+        if (quote != '\'') {
             in.setstate(std::ios::failbit);
             return in;
         }
@@ -144,19 +123,16 @@ namespace nspace
         in >> dest.ref;
 
         in >> quote;
-        if (quote != '\'')
-        {
+        if (quote != '\'') {
             in.setstate(std::ios::failbit);
             return in;
         }
         return in;
     }
 
-    std::istream& operator>>(std::istream& in, RationalIO&& dest)
-    {
+    std::istream& operator>>(std::istream& in, RationalIO&& dest) {
         std::istream::sentry sentry(in);
-        if (!sentry)
-        {
+        if (!sentry) {
             return in;
         }
 
@@ -168,48 +144,39 @@ namespace nspace
         in >> denominator;
         in >> DelimiterIO{ ':' } >> DelimiterIO{ ')' };
 
-        if (in && denominator >= 0)
-        {
+        if (in && denominator >= 0) {
             dest.ref = { numerator, denominator };
         }
-        else
-        {
+        else {
             in.setstate(std::ios::failbit);
         }
         return in;
     }
 
-    std::istream& operator>>(std::istream& in, StringIO&& dest)
-    {
+    std::istream& operator>>(std::istream& in, StringIO&& dest) {
         std::istream::sentry sentry(in);
-        if (!sentry)
-        {
+        if (!sentry) {
             return in;
         }
         return std::getline(in >> DelimiterIO{ '"' }, dest.ref, '"');
     }
 
-    std::istream& operator>>(std::istream& in, LabelIO&& dest)
-    {
+    std::istream& operator>>(std::istream& in, LabelIO&& dest) {
         std::istream::sentry sentry(in);
-        if (!sentry)
-        {
+        if (!sentry) {
             return in;
         }
 
         std::string data = "";
-        if ((in >> data) && (data != dest.exp))
-        {
+        if ((in >> data) && (data != dest.exp)) {
             in.setstate(std::ios::failbit);
         }
         return in;
     }
 
-    std::istream& operator>>(std::istream& in, DataStruct& dest)
-    {
+    std::istream& operator>>(std::istream& in, DataStruct& dest) {
         std::istream::sentry sentry(in);
-        if (!sentry)
-        {
+        if (!sentry) {
             return in;
         }
 
@@ -218,69 +185,52 @@ namespace nspace
         bool flag_key2 = false;
         bool flag_key3 = false;
 
-        in >> DelimiterIO{ '(' }>> DelimiterIO{ ':' };
+        in >> DelimiterIO{ '(' } >> DelimiterIO{ ':' };
 
-        while (in)
-        {
-
-            if (in.peek() == ')')
-            {
+        while (in) {
+            if (in.peek() == ')') {
                 in.ignore();
                 break;
             }
 
-      
-
             std::string field;
 
-            if (!(in >> field))
-            {
+            if (!(in >> field)) {
                 break;
             }
 
-            if (field == "key1")
-            {
+            if (field == "key1") {
                 in >> CharIO{ input.key1 } >> DelimiterIO{ ':' };
                 flag_key1 = true;
             }
-            else if (field == "key2")
-            {
+            else if (field == "key2") {
                 in >> RationalIO{ input.key2 } >> DelimiterIO{ ':' };
                 flag_key2 = true;
             }
-            else if (field == "key3")
-            {
+            else if (field == "key3") {
                 in >> StringIO{ input.key3 } >> DelimiterIO{ ':' };
                 flag_key3 = true;
             }
-            else
-            {
+            else {
                 in.setstate(std::ios::failbit);
                 break;
             }
-
         }
 
-        if (in && flag_key1 && flag_key2 && flag_key3)
-        {
+        if (in && flag_key1 && flag_key2 && flag_key3) {
             dest = input;
         }
-        else
-        {
+        else {
             in.setstate(std::ios::failbit);
         }
-
 
         return in;
     }
 
-
-    std::ostream& operator<<(std::ostream& out, const DataStruct& src)
-    {
+    std::ostream& operator<<(std::ostream& out, const DataStruct& src) {
         std::ostream::sentry sentry(out);
-        if (!sentry)
-        {
-            return  out;
+        if (!sentry) {
+            return out;
         }
 
         iofmtguard fmtguard(out);
@@ -294,30 +244,25 @@ namespace nspace
         width_(s.width()),
         fill_(s.fill()),
         precision_(s.precision()),
-        fmt_(s.flags())
-    {}
+        fmt_(s.flags()) {
+    }
 
-    iofmtguard::~iofmtguard()
-    {
+    iofmtguard::~iofmtguard() {
         s_.width(width_);
         s_.fill(fill_);
         s_.precision(precision_);
         s_.flags(fmt_);
     }
 
-    bool compare(const DataStruct& a, const DataStruct& b)
-    {
-        if (a.key1 != b.key1)
-        {
+    bool compare(const DataStruct& a, const DataStruct& b) {
+        if (a.key1 != b.key1) {
             return a.key1 < b.key1;
         }
         double a_rational = static_cast<double>(a.key2.first) / a.key2.second;
         double b_rational = static_cast<double>(b.key2.first) / b.key2.second;
-        if (a_rational != b_rational)
-        {
-            return  a_rational < b_rational;
+        if (a_rational != b_rational) {
+            return a_rational < b_rational;
         }
         return a.key3.length() < b.key3.length();
     }
-
 }
