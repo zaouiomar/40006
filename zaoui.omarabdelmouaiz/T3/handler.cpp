94e432f73c480279b68ac7454a156624c5a62416
zaouiomar <135237973+zaouiomar@users.noreply.github.com>
2025-10-16 07:53:18 +0300

    Initial commit for T3

---
+++ zaoui.omarabdelmouaiz/T3/handler.cpp
@@ -0,0 +1,261 @@
+#include "handler.hpp"
+#include "scope_guard.hpp"
+
+template< typename Pred >
+void omar::areaOddEven(const std::vector< Polygon >& data, std::ostream& out, Pred predicate)
+{
+  std::vector< double > areas;
+  std::vector< Polygon > polysByPredicate;
+
+  std::copy_if(data.begin(), data.end(), std::back_inserter(polysByPredicate), predicate);
+  std::transform(polysByPredicate.begin(), polysByPredicate.end(), std::back_inserter(areas), calcArea);
+  double totalArea = std::accumulate(areas.begin(), areas.end(), 0.0);
+  areaOut(totalArea, out);
+}
+
+void omar::areaMean(const std::vector< Polygon >& data, std::ostream& out)
+{
+  if (data.size() == 0)
+  {
+    throw std::logic_error("Invalid query in AREA MEAN.");
+  }
+
+  std::vector< double > areas;
+  std::transform(data.begin(), data.end(), std::back_inserter(areas), calcArea);
+
+  double totalArea = std::accumulate(areas.begin(), areas.end(), 0.0);
+  double meanArea = totalArea / data.size();
+  areaOut(meanArea, out);
+}
+
+void omar::areaVertices(const std::vector< Polygon >& data, std::ostream& out, size_t vertices)
+{
+  using namespace std::placeholders;
+
+  std::vector< double > areas;
+  std::vector< Polygon > matchingPolygons;
+
+  auto isPolyOfSizeBind = std::bind(isPolygonOfSize, _1, std::ref(vertices));
+  std::copy_if(data.begin(), data.end(), std::back_inserter(matchingPolygons), isPolyOfSizeBind);
+  std::transform(matchingPolygons.begin(), matchingPolygons.end(), std::back_inserter(areas), calcArea);
+
+  double totalArea = std::accumulate(areas.begin(), areas.end(), 0.0);
+  areaOut(totalArea, out);
+}
+
+void omar::area(const std::vector< Polygon >& data, std::istream& in, std::ostream& out)
+{
+  using namespace std::placeholders;
+
+  std::string subcommand;
+  in >> subcommand;
+  if (in.fail())
+  {
+    throw std::logic_error("Input failed in AREA.");
+  }
+
+  std::map< std::string, std::function< void() > > subCmds;
+  using functionBoolPoly = std::function< bool(const Polygon&) >;
+  subCmds["ODD"] = std::bind(areaOddEven< functionBoolPoly >, std::ref(data), std::ref(out), isPolygonOdd);
+  subCmds["EVEN"] = std::bind(areaOddEven< functionBoolPoly >, std::ref(data), std::ref(out), isPolygonEven);
+  subCmds["MEAN"] = std::bind(areaMean, std::ref(data), std::ref(out));
+  try
+  {
+    subCmds.at(subcommand)();
+  }
+  catch (const std::out_of_range& e)
+  {
+    if (isStringNumeric(subcommand))
+    {
+      size_t vertices = std::stoull(subcommand); //stoull : st = string to...oull = unsigned long long
+      if (vertices < 3)
+      {
+        throw std::logic_error("Invalid query in AREA.");
+      }
+      areaVertices(data, out, vertices);
+    }
+    else
+    {
+      throw std::logic_error("Unknown command.");
+    }
+  }
+}
+
+void omar::minMaxArea(const std::vector< Polygon >& data, std::ostream& out, const std::string& command)
+{
+  Polygon resultingPolygon;
+  std::map< std::string, std::function< Polygon() > > minOrMax;
+  minOrMax["min"] = std::bind(minElement, std::cref(data), compareAreas);
+  minOrMax["max"] = std::bind(maxElement, std::cref(data), compareAreas);
+  try
+  {
+    resultingPolygon = minOrMax.at(command)();
+  }
+  catch (const std::out_of_range& e)
+  {
+    throw std::logic_error("minMaxArea failed.");
+  }
+
+  areaOut(calcArea(resultingPolygon), out);
+}
+
+void omar::minMaxVertices(const std::vector< Polygon >& data, std::ostream& out, const std::string& command)
+{
+  Polygon resultingPolygon;
+  std::map< std::string, std::function< Polygon() > > minOrMax;
+  minOrMax["min"] = std::bind(minElement, std::cref(data), compareVertices);
+  minOrMax["max"] = std::bind(maxElement, std::cref(data), compareVertices);
+  try
+  {
+    resultingPolygon = minOrMax.at(command)(); 
+  }
+  catch (const std::out_of_range& e)
+  {
+    throw std::logic_error("minMaxVertices failed.");
+  }
+
+  StreamGuard guard(out);
+  out << getVertices(resultingPolygon) << '\n';
+}
+
+void omar::minMax(const std::vector< Polygon >& data, std::istream& in, std::ostream& out, const std::string& command)
+{
+  using namespace std::placeholders;
+
+  std::string subcommand;
+  in >> subcommand;
+  if (in.fail() || data.size() == 0)
+  {
+    throw std::logic_error("Input failed in MIN/MAX.");
+  }
+
+  std::map< std::string, std::function< void() > > subCmds;
+  subCmds["AREA"] = std::bind(minMaxArea, std::ref(data), std::ref(out), std::cref(command));
+  subCmds["VERTEXES"] = std::bind(minMaxVertices, std::ref(data), std::ref(out), std::cref(command));
+  try
+  {
+    subCmds.at(subcommand)();
+  }
+  catch (const std::out_of_range& e)
+  {
+    throw std::logic_error("Unknown command.");
+  }
+}
+
+void omar::countEven(const std::vector< Polygon >& data, std::ostream& out)
+{
+  using namespace std::placeholders;
+  auto isPolygonEvenBind = std::bind(isPolygonEven, _1);
+  out << std::count_if(data.begin(), data.end(), isPolygonEvenBind) << '\n';
+}
+
+void omar::countOdd(const std::vector< Polygon >& data, std::ostream& out)
+{
+  using namespace std::placeholders;
+  auto isPolygonOddBind = std::bind(isPolygonOdd, _1);
+  out << std::count_if(data.begin(), data.end(), isPolygonOddBind) << '\n';
+}
+
+void omar::countVertices(const std::vector< Polygon >& data, std::ostream& out, size_t givenSize)
+{
+  using namespace std::placeholders;
+  auto isPolygonOfSizeBind = std::bind(isPolygonOfSize, _1, givenSize);
+  out << std::count_if(data.begin(), data.end(), isPolygonOfSizeBind) << '\n';
+}
+
+void omar::count(const std::vector< Polygon >& data, std::istream& in, std::ostream& out)
+{
+  std::map< std::string, std::function< void() > > subCmds;
+  subCmds["EVEN"] = std::bind(countEven, std::cref(data), std::ref(out));
+  subCmds["ODD"] = std::bind(countOdd, std::cref(data), std::ref(out));
+
+  std::string subcommand;
+  in >> subcommand;
+  if (in.fail())
+  {
+    throw std::logic_error("Input failed in COUNT.");
+  }
+
+  try
+  {
+    subCmds.at(subcommand)();
+  }
+  catch (const std::out_of_range& e)
+  {
+    if (isStringNumeric(subcommand))
+    {
+      size_t vertices = std::stoull(subcommand);
+      if (vertices < 3)
+      {
+        throw std::logic_error("Invalid query in COUNT.");
+      }
+      countVertices(data, out, std::stoull(subcommand));
+    }
+    else
+    {
+      throw std::logic_error("Unknown command.");
+    }
+  }
+}
+
+void omar::rmecho(std::vector< Polygon >& data, std::istream& in, std::ostream& out)
+{
+  using namespace std::placeholders;
+
+  Polygon rmPolygon;
+  in >> rmPolygon;
+  if (in.fail() || in.peek() != '\n')
+  {
+    throw std::logic_error("Input failed in RMECHO.");
+  }
+
+  size_t oldSize = data.size();
+  auto helperBind = std::bind(rmEchoHelper, rmPolygon, _1, _2);
+  auto new_end = std::unique(data.begin(), data.end(), helperBind);
+  data.erase(new_end, data.end()); 
+  size_t removed = oldSize - data.size();
+
+  StreamGuard guard(out);
+  out << removed << '\n';
+}
+
+void omar::inframe(const std::vector< Polygon >& data, std::istream& in, std::ostream& out)
+{
+  using namespace std::placeholders;
+
+  Polygon inframePoly;
+  in >> inframePoly;
+  if (in.fail() || in.peek() != '\n') //Checks if there's extra unexpected input after reading the polygon
+  {
+    throw std::logic_error("Input failed in INFRAME.");
+  }
+
+  Borders polysBox = getMaxPolygonBox(data);
+  auto isPointInBordersBind = std::bind(isPointInBorders, _1, std::cref(polysBox));
+  bool inside = std::all_of(inframePoly.points.begin(), inframePoly.points.end(), isPointInBordersBind);
+  // std::all_of: Returns true only if ALL points satisfy the condition
+  StreamGuard guard(out);
+  if (inside)
+  {
+    out << "<TRUE>\n";
+  }
+  else
+  {
+    out << "<FALSE>\n";
+  }
+}
+
+omar::commandMap omar::mapCommandHandlers(std::vector< Polygon >& data)
+{
+  using namespace std::placeholders; // Brings the placeholder names _1, _2, _3
+  commandMap cmds; //creates a map named cmds
+  const std::string minCommand = "min";
+  const std::string maxCommand = "max";
+  cmds["AREA"] = std::bind(area, std::cref(data), _1, _2);
+  cmds["MAX"] = std::bind(minMax, std::cref(data), _1, _2, maxCommand);
+  cmds["MIN"] = std::bind(minMax, std::cref(data), _1, _2, minCommand);
+  cmds["COUNT"] = std::bind(count, std::cref(data), _1, _2);
+  cmds["RMECHO"] = std::bind(rmecho, std::ref(data), _1, _2); // read-write access
+  cmds["INFRAME"] = std::bind(inframe, std::cref(data), _1, _2); // read-write access
+  return cmds;
+}
---
+++ zaoui.omarabdelmouaiz/T3/shapes.cpp
@@ -0,0 +1,100 @@
+#include "shapes.hpp"
+#include <algorithm>
+#include <iterator>
+#include <functional>
+#include "scope_guard.hpp"
+#include "delimiter.hpp"
+
+std::istream& omar::operator>>(std::istream& in, Point& dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
+
+  Point newPoint;
+  in >> DelimiterIO{'('};
+  in >> newPoint.x;
+  in >> DelimiterIO{';'};
+  in >> newPoint.y;
+  in >> DelimiterIO{')'};
+  if (in)
+  {
+    dest = newPoint;
+  }
+  return in;
+}
+
+std::ostream& omar::operator<<(std::ostream& out, const Point& src)
+{
+  std::ostream::sentry sentry(out);
+  if (!sentry)
+  {
+    return out;
+  }
+
+  omar::StreamGuard guard(out);
+  out << "(" << src.x << ";" << src.y << ")";
+  return out;
+}
+
+std::istream& omar::operator>>(std::istream& in, Polygon& dest)
+{
+  using namespace std::placeholders;
+
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
+
+  size_t pointsAmount;
+  in >> pointsAmount;
+  if (!in || pointsAmount < 3)
+  {
+    in.setstate(std::ios::failbit);
+    return in;
+  }
+
+  using istreamPnt = std::istream_iterator< Point >;
+  std::vector< Point > newPoints(pointsAmount, Point{0, 0});
+  std::copy_n(istreamPnt(in), pointsAmount, newPoints.begin());
+  if (in && pointsAmount == newPoints.size())
+  {
+    dest.points = std::move(newPoints);
+  }
+  else
+  {
+    in.setstate(std::ios::failbit);
+  }
+  return in;
+}
+
+std::ostream& omar::operator<<(std::ostream& out, const Polygon& src)
+{
+  std::ostream::sentry sentry(out);
+  if (!sentry)
+  {
+    return out;
+  }
+
+  omar::StreamGuard guard(out);
+  using ostreamPnt = std::ostream_iterator< Point >;
+  std::copy(src.points.begin(), src.points.end(), ostreamPnt{out, " "}); // Automatic spacing with iterator
+  return out;
+}
+
+bool omar::operator==(const Point& lhs, const Point& rhs)
+{
+  return (lhs.x == rhs.x && lhs.y == rhs.y);
+}
+
+bool omar::operator==(const Polygon& lhs, const Polygon& rhs)
+{
+  if (lhs.points.size() != rhs.points.size())
+  {
+    return false;
+  }
+  return std::is_permutation(lhs.points.begin(), lhs.points.end(), rhs.points.begin());
+}
